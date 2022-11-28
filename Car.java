9f1156ba6225e27d9b7e0521377e49da3f5bb3e1
Feev0 <matthewreichmann@me.com>
2022-11-28 09:11:22 +0100

    carcomment

--- Car.java
+++ Car.java
@@ -1,4 +1,6 @@
-
+// Car is just a subclass of Obstacle.
+// The only thing that is different about it is that it is the only object that receives a checkWin() method.
+// Since the car is the only Obstacle that can actually win the game.
 
 public class Car extends Obstacle {
 
